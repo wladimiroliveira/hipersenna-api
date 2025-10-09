e463b2f8af064052e95e3fe915a3a2ac83e10ea8
Wladimir Moraes Oliveira <76710345+wladimiroliveira@users.noreply.github.com>
2025-10-09 13:27:36 +0000

    habilite cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import fastify from "fastify";
-import routes from "./routes.js"
-import swagger from "@fastify/swagger"
-import swaggerUI from "@fastify/swagger-ui"
+import routes from "./routes.js";
+import { fastifyCors } from "@fastify/cors";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 
 const app = fastify();
 
-await app.register(import('@fastify/swagger'))
+await app.register(fastifyCors, {
+  origin: ["*"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
 
-declare module "fastify"
+await app.register(import("@fastify/swagger"));
+
+declare module "fastify";
 
 app.register(routes);
 
-app.listen({port: 3333, host: "0.0.0.0"}).then(() => {
-    console.log('Server is running');
-}) 
-
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("Server is running");
+});
